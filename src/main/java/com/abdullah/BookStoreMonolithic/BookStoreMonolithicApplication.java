9bfdd6aaf5bc8b06743e641aff3fd06f034dc055
Abdullah <abdullah.theeb@outlook.com>
2023-11-11 04:47:01 +0300

    h

--- src/main/java/com/abdullah/BookStoreMonolithic/BookStoreMonolithicApplication.java
+++ src/main/java/com/abdullah/BookStoreMonolithic/BookStoreMonolithicApplication.java
@@ -8,6 +8,14 @@ public class BookStoreMonolithicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreMonolithicApplication.class, args);
+		/// write a method to get all the books
+		/// write a method to get a book by id	
+		/// write a method to add a book
+		/// write a method to update a book
+		/// write a method to delete a book
+
 	}
+
+	/// method to get all the books
 
 }
